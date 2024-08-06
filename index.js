1f5f8225ffabd7309f9fedff22f3e9fa83270797
avianurag721 <anurag.721721@gmail.com>
2024-08-06 11:16:19 +0530

    cors origin changed to https://fahhealthcare.in

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(
 	cors({
 		// origin:"http://localhost:3000",
-		origin:"https://fahhospital.netlify.app",
+		origin:"https://fahhealthcare.in",
 		credentials:true,
 	})
 )
